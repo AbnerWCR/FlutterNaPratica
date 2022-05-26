f50483e02b5f363099ec3bb2f8eb19f85835c64b
AbnerWCR <abnerwcrodrigues@gmail.com>
2022-05-26 17:56:21 -0300

    variable initialization types

--- ep01/main.dart
+++ ep01/main.dart
@@ -1,3 +1,16 @@
+//Objetivos:
+//Criar uma lista de lembretes
+//Adicionar e remover itens da lista
+
+//Obs:
+// [] - quando usa colchetes é a inicialização de uma lista;
+// {} - quando usa chaves é a inicialização de um map ou array;
+// final - define um valor imutável (em tempo de execução);
+// const - define um valor imutável (em tempo de compilação);
+
 void main() {
-  print("I love you Beatriz S2");
+  var listReminderDynamic = [];
+  List listReminderType;
+  final listRemiderFinal = [];
+
 }
