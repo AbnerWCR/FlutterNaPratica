b10ef1777db0aa5fda8a38be36bb2a6861f3dcc5
AbnerWCR <abnerwcrodrigues@gmail.com>
2022-06-03 14:16:08 -0300

    add a firs business rules

---
+++ ep02/main.dart
@@ -0,0 +1,59 @@
+//create Login:
+//validate email
+//validate
+
+//go to Home
+
+void main() {
+  final isValid = validate(username: "abner", password: "abner123");
+
+  if (stringIsNullOrEmpty(isValid)) {
+    login(username: "abner", password: "abner123");
+  }
+}
+
+void login({
+  required String username, 
+  required String password
+}) {
+  validateLogin(username: username, password: password);
+}
+
+String? validate({
+  required String username, 
+  required String password
+}) {
+  final usernameIsValid = validateUsername(username);
+  final passwordIsValid = validatePassword(password);
+
+  if (!passwordIsValid) {
+    return "password must be 6 characters";
+  } 
+  
+  if (!usernameIsValid) {
+    return "username can't be null";
+  }
+
+  return null;
+}
+
+bool validateUsername(String? username) => username != null && username.length > 0;
+
+bool validatePassword(String? password)  => password != null && password.length > 6;
+
+void validateLogin(
+  {
+  required String username, 
+  required String password
+}) {
+  print("Connecting to the server");
+  print("welcome $username");
+}
+
+bool stringIsNullOrEmpty(String? value) {
+  if (value == null || value.isEmpty)
+  {
+    return true;
+  }
+  return false;
+}
