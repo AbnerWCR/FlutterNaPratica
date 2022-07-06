ece934e0bdcbe98be9e8b9b257c82e9a2c39533d
AbnerWCR <abnerwcrodrigues@gmail.com>
2022-07-05 21:33:04 -0300

    login page with validation

--- ep02/auth/lib/main.dart
+++ ep02/auth/lib/main.dart
@@ -1,63 +1,105 @@
-//create Login:
-//validate email
-//validate
+// ignore_for_file: prefer_const_constructors
 
-//go to Home
+import 'package:flutter/material.dart';
 
 void main() {
-  final isValid = validate(username: "abner", password: "abner123");
-
-  if (stringIsNullOrEmpty(isValid)) {
-    login(username: "abner", password: "abner123");
-  }
+  runApp(MaterialApp(
+    home: LoginPage(),
+    theme: ThemeData(primarySwatch: Colors.deepPurple),
+  ));
 }
 
-void login({
-  required String username, 
-  required String password
-}) {
-  final response = validateLogin(username: username, password: password);
-  if (response) {
-    print("Open HomePage");
-  }
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _LoginPageState();
 }
 
-String? validate({
-  required String username, 
-  required String password
-}) {
-  final usernameIsValid = validateUsername(username);
-  final passwordIsValid = validatePassword(password);
+class _LoginPageState extends State<LoginPage> {
+  String? username;
+  String? password;
+  final formKey = GlobalKey<FormState>();
 
-  if (!passwordIsValid) {
-    return "password must be 6 characters";
-  } 
-  
-  if (!usernameIsValid) {
-    return "username can't be null";
-  }
+  String? validateUsername(String? username) =>
+      username != null && username.isNotEmpty 
+          ? null
+          : "username can't be null";
 
-  return null;
-}
+  String? validatePassword(String? password) =>
+      password != null && password.length >= 6
+          ? null
+          : "password must be 6 characters";
 
-bool validateUsername(String? username) => username != null && username.length > 0;
-
-bool validatePassword(String? password)  => password != null && password.length > 6;
-
-bool validateLogin(
-  {
-  required String username, 
-  required String password
-}) {
-  print("Connecting to the server");
-  print("welcome $username");
-  return true;
-}
-
-bool stringIsNullOrEmpty(String? value) {
-  if (value == null || value.isEmpty)
-  {
+  bool login({required String username, required String password}) {
+    print("Connecting to the server");
+    print("welcome $username");
+    print("Login");
     return true;
   }
-  return false;
+
+  bool validate() {
+    final form = formKey.currentState!;
+
+    if (form.validate()) {
+      form.save();
+
+      return true;
+    } 
+
+    return false;
+  }
+
+  bool stringIsNullOrEmpty(String? value) {
+    if (value == null || value.isEmpty) {
+      return true;
+    }
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 32),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: "Username"),
+                validator: (value) => validateUsername(value),
+                onSaved: (value) => username = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Password"),
+                validator: (value) => validatePassword(value),
+                onSaved: (value) => password = value,
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.deepPurple),
+                  ),
+                  onPressed: () {
+                    if (validate()) {
+                      login(username: username!, password: password!);
+                    }
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white),
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
